93fa8d7ce7854150412dcb11e42c6a627a9588d5
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2025-05-15 10:18:30 +0100

    initialize modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AgencyModule } from './agency/agency.module';
+import { CarModule } from './car/car.module';
+import { ReviewModule } from './review/review.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AgencyModule, CarModule, ReviewModule, BookingModule],
   controllers: [AppController],
   providers: [AppService],
 })
