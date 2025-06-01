92dc01909d51c1a3ab1d023fb6b02045956d8ae8
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2025-06-01 10:32:25 +0100

    add the auth part of the agency

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,16 +2,33 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UserService } from '../user/user.service';
+import { AgencyService } from '../agency/agency.service';
 
 @Injectable()
 export class AuthService {
     constructor(
         private usersService: UserService,
+        private agencyService: AgencyService,
         private jwtService: JwtService,
     ) { }
 
     async validateUser(email: string, password: string): Promise<any> {
         const user = await this.usersService.findByEmail(email);
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        if (user && await bcrypt.compare(password, user.password)) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async validateAgency(email: string, password: string): Promise<any> {
+        const user = await this.agencyService.findByEmail(email);
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
         if (user && await bcrypt.compare(password, user.password)) {
             const { password, ...result } = user;
             return result;
