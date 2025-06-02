37e88f1f17f496dd3efe3d467fb39b9ac056b940
medini <mohameddhia.medini@insat.ucar.tn>
2025-06-02 13:01:59 +0100

    Decorators and Authorization

--- backend/src/booking/booking.module.ts
+++ backend/src/booking/booking.module.ts
@@ -4,9 +4,10 @@ import { Booking } from "src/booking/entities/booking.entity";
 import { BookingService } from "./booking.service";
 import { BookingController } from "./booking.controller";
 import { EventsModule } from "../events/events.module";
+import { Car } from "../car/entities/car.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking]), EventsModule],
+  imports: [TypeOrmModule.forFeature([Booking ,Car ]), EventsModule],
   controllers: [BookingController],
   providers: [BookingService],
   exports: [BookingService],
--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -4,6 +4,9 @@ import { EventsService } from './events.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UseGuards } from '@nestjs/common';
 import { Agency } from 'src/auth/decorators/auth.decorators';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enums/role.enum';
+import { RolesGuard } from 'src/auth/guards/roles/roles.guard';
 
 @Controller('events')
 @UseGuards(JwtAuthGuard)
@@ -11,16 +14,18 @@ export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
 
+  @Roles(Role.AGENCY)
+  @UseGuards(RolesGuard)
   @Sse('bookings/agency')
   subscribeToAgencyBookings(
     @Agency() agency
   ): Observable<MessageEvent> {
-    if(!agency){
-      return new Observable();
-    }
     return this.eventsService.subscribeToAgencyBookings(agency.agencyId);
   }
 
+
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
   @Sse('bookings')
   subscribeToBookings(): Observable<MessageEvent> {
     return this.eventsService.subscribeToBookingCreated();
